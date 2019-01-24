07c3765ed43c685a671fac58128e6e6bf9f8f666
Tomasz Buchta <tomasz.buchta@hotmail.com>
2019-01-24 20:20:24 +0100

    Change association type to use intermediate model

--- project/app/models/country.rb
+++ project/app/models/country.rb
@@ -3,7 +3,8 @@ class Country < ApplicationRecord
 
   has_many :location_groups
 
-  has_and_belongs_to_many :target_groups
+  has_many :countries_target_groups
+  has_many :target_groups, through: :countries_target_groups
 
   validates :code, presence: true, uniqueness: true
   validates :panel_provider, presence: true
--- project/app/models/target_group.rb
+++ project/app/models/target_group.rb
@@ -2,7 +2,8 @@ class TargetGroup < ApplicationRecord
   belongs_to :parent, class_name: "TargetGroup", optional: true
   belongs_to :panel_provider
 
-  has_and_belongs_to_many :countries
+  has_many :countries_target_groups
+  has_many :countries, through: :countries_target_groups
 
   def root?
     parent_id.nil?
