fd8c5acd7cff6742b22c4ff1ad89387ba849013f
Tomasz Buchta <tomasz.buchta@hotmail.com>
2019-01-25 19:13:34 +0100

    Seed LocationGroup

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -29,7 +29,19 @@ LOCATIONS = [
   { name: "Seattle" }
 ].freeze
 
+[
+  { secret_code: "AAA111", name: "Tenis players" },
+  { secret_code: "AAA112", name: "Football players" },
+  { secret_code: "AAA113", name: "Hockey players" },
+  { secret_code: "AAA114", name: "Archers" },
+  { secret_code: "AAA115", name: "Martial arts practitioners" },
+  { secret_code: "AAA116", name: "Runners" },
+  { secret_code: "AAA117", name: "Volleyball players" }
+]
+
 PANEL_PROVIDERS_CODES.each { |panel_provider_code| PanelProvider.create!(code: panel_provider_code) }
+
+panel_providers = PanelProvider.all.to_a << PanelProvider.last
 
 COUNTRIES.each do |country|
   Country.create!(
@@ -38,10 +50,28 @@ COUNTRIES.each do |country|
   )
 end
 
+countries = Country.all.to_a << Country.last
+
 LOCATIONS.each do |location|
   Location.create!(
     name: location.fetch(:name),
     external_id: SecureRandom.uuid,
     secret_code: SecureRandom.hex(64)
+  )
+end
+
+LOCATION_GROUPS = [
+  { name: "The West" },
+  { name: "The South" },
+  { name: "The Midwest" },
+  { name: "The Norht-East" }
+].freeze
+
+panel_providers
+  .zip(countries, LOCATION_GROUPS) do |panel_provider, country, location_group|
+  LocationGroup.create!(
+    panel_provider: panel_provider,
+    country: country,
+    name: location_group[:name]
   )
 end
