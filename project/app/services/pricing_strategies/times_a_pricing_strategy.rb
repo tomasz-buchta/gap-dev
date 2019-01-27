0685d1184d5efa5e9752d6d6ffbfde35f918ad41
Tomasz Buchta <tomasz.buchta@hotmail.com>
2019-01-27 19:49:30 +0100

    Add times-a panel provider pricing strategy

--- project/app/services/pricing_strategies/times_a_pricing_strategy.rb
+++ project/app/services/pricing_strategies/times_a_pricing_strategy.rb
@@ -1,5 +1,19 @@
-class TimesAPricingStrategy
-  def call
+require "dry/monads/result"
 
+module PricingStrategies
+  class TimesAPricingStrategy
+    include Dry::Monads::Result::Mixin
+
+    URL = "http://time.com/".freeze
+    CHARACTER_TO_COUNT = "a".freeze
+
+    def call
+      response = HTTParty.get(URL)
+      if response.success?
+        Success(Nokogiri::HTML(response).text.count(CHARACTER_TO_COUNT))
+      else
+        Failure("Cannot fetch #{URL}")
+      end
+    end
   end
 end
